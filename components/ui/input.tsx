35d32dcc99db07e63797b1ecbdb142bc2c726b52
babatunde tolulope <babsbright69@gmail.com>
2025-03-14 11:06:42 +0100

    Edited the input component

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -1,6 +1,8 @@
 import * as React from "react"
 
 import { cn } from "@/lib/utils"
+import { Eye, EyeClosed } from "lucide-react";
+import { useState } from "react";
 
 function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   return (
@@ -18,4 +20,36 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
   )
 }
 
-export { Input }
+function Input2({ className, type, ...props }: React.ComponentProps<"input">) {
+  const [passwordShow, setPasswordShow] = useState<boolean>(false);
+  return (
+    <div
+      className={cn(
+        "border-crown-border-grey items-center justify-between file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        className
+      )}
+    >
+      <input
+        type={passwordShow ? "text" : "password"}
+        data-slot="input"
+        className="border-none focus:outline-none
+         file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm"
+        {...props}
+      />
+
+      <span
+        className=" text-crown-black"
+        onClick={() => setPasswordShow(!passwordShow)}
+      >
+        {passwordShow ? (
+          <Eye className="size-5" />
+        ) : (
+          <EyeClosed className="size-5" />
+        )}
+      </span>
+    </div>
+  );
+}
+export { Input, Input2 };
