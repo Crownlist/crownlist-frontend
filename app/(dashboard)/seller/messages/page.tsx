4e194966d283d8e6ac0c02881f9cb438e9218b25
toyman-tech <kolatoyin94@gmail.com>
2025-10-17 05:30:42 +0100

    new commit

--- app/(dashboard)/seller/messages/page.tsx
+++ app/(dashboard)/seller/messages/page.tsx
@@ -1,11 +1,14 @@
-import NoMessagePage from "@/components/Home/noMessage";
+
+import Message from "@/components/Home/Message";
 
 
 
 export default function Home() {
     return (
-        <div className="flex w-full h-full justify-center items-center align-middle">
-                <NoMessagePage />
+        <div className="flex w-full h-full ">
+            <div className="flex w-full h-full mx-auto  py-6 max-md:px-5">
+                <Message/>
+            </div>
         </div>
     )
 }
